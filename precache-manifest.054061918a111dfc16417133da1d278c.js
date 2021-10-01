ee29cdc0262a56b086b5a10cb1f1cf7f6776ffb5
Max <maxperezmas@gmail.com>
2021-09-30 23:30:45 -0400

    Updates

---
+++ precache-manifest.054061918a111dfc16417133da1d278c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cd677b77e4312274425ee1940ea787",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "f8a42bd2d6ea2690c67f",
+    "url": "/react-quiz/static/js/2.2418c81b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.2418c81b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44cf7ba35b5cc3ea97c",
+    "url": "/react-quiz/static/js/main.b0ba93da.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/react-quiz/static/media/nattu-adnan-unsplash.c11f97a3.jpg"
+  }
+]);
